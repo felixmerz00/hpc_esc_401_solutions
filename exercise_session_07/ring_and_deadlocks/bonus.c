730f914cdb1dc4e193dc98b2c4c4ed77b425b61d
Felix Merz <felixmerz00@gmail.com>
2023-11-04 12:03:29 +0100

    ex7.1 bonus

--- exercise_session_07/ring_and_deadlocks/bonus.c
+++ exercise_session_07/ring_and_deadlocks/bonus.c
@@ -7,9 +7,11 @@ int main(int argc, char** argv) {
   MPI_Init(NULL, NULL);
 
   // Get the number of processes and rank of the process
-  int source_rank, rneigh_rank;
+  int size, my_rank, lneigh_rank, rneigh_rank;
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-  int dims[1] = {4};
+  int dims[1] = {size};
   int periods[1] = {1};
   // int MPI_Cart_create(MPI_Comm comm_old, int ndims, int *dims, int *periods, int reorder, MPI_Comm *comm_cart);
   MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periods, 1, &comm_cart);
@@ -18,7 +20,7 @@ int main(int argc, char** argv) {
   int MPI_Cart_shift(comm_cart, 0, 1, &source_rank, &rneigh_rank);
 
 
-  printf("I am processor %d. My right neighbour is %d.\n", source_rank, rneigh_rank);
+  printf("I am processor %d. My left neighbour is %d. My right neighbour is %d.\n", lneigh_rank, rneigh_rank);
 
   // Finalize the MPI environment.
   MPI_Finalize();
