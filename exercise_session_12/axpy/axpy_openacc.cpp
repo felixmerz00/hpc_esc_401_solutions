0cbd8be212fc6bfc1f30aa940d0803873c58bfb8
Felix Merz <felixmerz00@gmail.com>
2023-12-08 14:13:28 +0100

    ex12.1

--- exercise_session_12/axpy/axpy_openacc.cpp
+++ exercise_session_12/axpy/axpy_openacc.cpp
@@ -18,7 +18,7 @@ void axpy_gpu(int n, double alpha, const double *x, double* y) {
     int i;
 
     // TODO: offload this loop to the GPU
-    #pragma acc parallel loop pcopyin(x[0:n]) pcopyout(y[0:n])
+    #pragma acc parallel loop pcopyin(x[0:n]) pcopy(y[0:n])
     {
         for(i = 0; i < n; ++i) {
             y[i] += alpha*x[i];
