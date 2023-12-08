ed335196797dc20309afff02cebf6f9aea1d5fa8
Felix Merz <felixmerz00@gmail.com>
2023-12-08 16:23:15 +0100

    ex12.2

--- exercise_session_12/basics/blur_openacc.cpp
+++ exercise_session_12/basics/blur_openacc.cpp
@@ -78,7 +78,7 @@ void blur_twice_gpu_nocopies(double *in , double *out , int n, int nsteps)
     double *buffer = malloc_host<double>(n);
 
     // TODO: move the data needed by the algorithm to the GPU
-    #pragma acc data pcopy(in[0:n]) pcreate(out[0:n]) pcreate(buffer[0:n])
+    #pragma acc data pcopy(in[0:n]) pcreate(out[0:n], buffer[0:n])
     {
         for (auto istep = 0; istep < nsteps; ++istep) {
             // TODO: offload this loop to the GPU
