10b5dbb5f9e00da2e67c4d6ca577cc901186cb0f
Felix Merz <felixmerz00@gmail.com>
2023-12-08 16:19:17 +0100

    ex12.2

--- exercise_session_12/basics/blur_openacc.cpp
+++ exercise_session_12/basics/blur_openacc.cpp
@@ -78,27 +78,25 @@ void blur_twice_gpu_nocopies(double *in , double *out , int n, int nsteps)
     double *buffer = malloc_host<double>(n);
 
     // TODO: move the data needed by the algorithm to the GPU
+    #pragma acc data pcopy(in[0:n]) pcreate(out[0:n]) pcreate(buffer[0:n])
     {
         for (auto istep = 0; istep < nsteps; ++istep) {
-            #pragma acc data pcopy(in[0:n])
-            {
-                // TODO: offload this loop to the GPU
-                #pragma acc parallel loop
-                for (auto i = 1; i < n-1; ++i) {
-                    buffer[i] = blur(i, in);
-                }
+            // TODO: offload this loop to the GPU
+            #pragma acc parallel loop
+            for (auto i = 1; i < n-1; ++i) {
+                buffer[i] = blur(i, in);
+            }
 
-                // TODO: offload this loop to the GPU
-                #pragma acc parallel loop
-                for (auto i = 2; i < n-2; ++i) {
-                    out[i] = blur(i, buffer);
-                }
+            // TODO: offload this loop to the GPU
+            #pragma acc parallel loop
+            for (auto i = 2; i < n-2; ++i) {
+                out[i] = blur(i, buffer);
+            }
 
-                // TODO: offload this loop to the GPU; can you try just the pointer assignment?
-                #pragma acc parallel loop
-                for (auto i = 0; i < n; ++i) {
-                    in[i] = out[i];
-                }
+            // TODO: offload this loop to the GPU; can you try just the pointer assignment?
+            #pragma acc parallel loop
+            for (auto i = 0; i < n; ++i) {
+                in[i] = out[i];
             }
         }
     }
